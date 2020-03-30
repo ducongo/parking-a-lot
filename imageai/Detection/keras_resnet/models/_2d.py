feeb3d58c89cd54bb60bc75da6b0f344dccbe5d2
parfait <parfait.ducongo@gmail.com>
2020-03-29 20:58:35 -0400

    removed faulty imageai folder and files

--- imageai/Detection/keras_resnet/models/_2d.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bd55804525d34b8a2f8b466b335742cf264dca7ee4fca6d902c03b4914ed0ab6
-size 9847
--- imageai/Detection/keras_resnet/models/__init__.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4e28e962762bb673c85a15079b0833927c58982f0e7905a85083ab1be2454471
-size 484
